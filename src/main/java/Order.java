76f75547e8085aaba2799d8ce7afd07114de3017
Bill Schofield <bill@billschofield.net>
2014-05-29 16:01:12 -0500

    created Pizza class and order has pizzas instead of quantity

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,16 +1,21 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Order {
 
     private final int price;
-    private int quantity;
+    private final ArrayList<Pizza> pizzas;
 
     public Order(OrderTaker orderTaker) throws IOException {
-        this.quantity = orderTaker.takeOrder();
+        int quantity = orderTaker.takeOrder();
+        pizzas = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+             pizzas.add(new Pizza());
+        }
         price = 6;
     }
 
     public int computeTotal() {
-        return price * this.quantity;
+        return price * pizzas.size();
     }
 }
